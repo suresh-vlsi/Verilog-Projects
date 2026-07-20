660d2ea307da533fc7fa813c9967b26ea64706c6
suresh-vlsi <kusumurusureshkumar21@gmail.com>
2026-07-20 14:13:12 +0530

    Add Half Adder RTL and Testbench

---
+++ 01_Combinational/Half_Adder/half_adder_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+
+module half_adder_tb;
+
+reg a, b;
+wire sum, carry;
+
+half_adder uut (
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+
+initial begin
+    $display("A B | SUM CARRY");
+    $display("----------------");
+
+    a = 0; b = 0; #10;
+    $display("%b %b |  %b    %b", a, b, sum, carry);
+
+    a = 0; b = 1; #10;
+    $display("%b %b |  %b    %b", a, b, sum, carry);
+
+    a = 1; b = 0; #10;
+    $display("%b %b |  %b    %b", a, b, sum, carry);
+
+    a = 1; b = 1; #10;
+    $display("%b %b |  %b    %b", a, b, sum, carry);
+
+    $finish;
+end
+
+endmodule
